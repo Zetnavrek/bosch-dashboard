d07ae5c0af03812f74515c5ed3f6d9f86f52d974
Zetnavrek <vykovi@gmail.com>
2018-09-13 10:29:12 -0500

    A few changes in App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
     this.handleGetData = this.handleGetData.bind(this);
   }
 
-  handleGetData(){
+  handleGetData() {
     fetch('https://connectorysolutions.com/talentfest/data/')
       .then(response => {
         return response.json()
@@ -29,7 +29,7 @@ class App extends Component {
         console.log(this);
         this.setState({ data: data[lastValues] });
         console.log(data[0].temperature);
-      }) 
+      })
   }
 
 
@@ -74,7 +74,7 @@ class App extends Component {
   //       //console.log(data[0].temperature);
   //     }) 
   // }
- 
+
 
   render() {
     // const {data} = this.state;
@@ -84,70 +84,70 @@ class App extends Component {
       <div className="App">
         <img className="img-fluid" src={colores} alt="" />
         <header className="container-fluid">
-        <div className="row">
-        <div className="col">
-        <img className="logo-bosch" src={logo} alt="" />
-        </div>
-        <div className="col name">
-            <h3>XDK Monitoring</h3>
+          <div className="row">
+            <div className="col">
+              <img className="logo-bosch" src={logo} alt="" />
             </div>
-        </div>
-        <div className="line"></div>
-        <div className="row">
-              <div className="col">
-                <div className="">
-                  <i className="fas fa-bars"></i>
-                </div>
-                </div>
-                <div className="col personal-info">
-                  <i className="far fa-life-ring"></i>
-                  <i className="fas fa-bell"></i>
-                  <i className="fas fa-chevron-down"></i>
-                  <img src={person} className="round" />
-                </div>
+            <div className="col name">
+              <h3>XDK Monitoring</h3>
+            </div>
+          </div>
+          <div className="line"></div>
+          <div className="row">
+            <div className="col">
+              <div className="">
+                <i className="fas fa-bars"></i>
               </div>
+            </div>
+            <div className="col personal-info">
+              <i className="far fa-life-ring"></i>
+              <i className="fas fa-bell"></i>
+              <i className="fas fa-chevron-down"></i>
+              <img src={person} className="round" />
+            </div>
+          </div>
         </header>
         <main>
-        <div className="sensors container">
-        <div className="row">
-          <div className="temperature sensor col-sm-12 col-md-3 col-lg-3">
-            <h2 className="col-12">Temperature</h2>
-            <span className="value col-12">{this.state.data.temperature}℃</span>
-            <div className="notification-sensor col-12">
-            {/* <Notification props={this.state.data.temperature}/> */}
-            <i className="fas fa-exclamation notification-icon col-12"></i>
-              <span className="col-12">Status</span>
+          <div className="sensors container">
+            <div className="row">
+              <div className="temperature sensor col-sm-12 col-md-3 col-lg-3">
+                <h2 className="col-12">Temperature</h2>
+                <span className="value col-12">{this.state.data.temperature}℃</span>
+                <div className="notification-sensor col-12">
+                  {/* <Notification props={this.state.data.temperature}/> */}
+                  <i className="fas fa-exclamation notification-icon col-12"></i>
+                  <span className="col-12">Status</span>
+                </div>
+              </div>
+              <div className="humidity sensor col-sm-12 col-md-3 col-lg-3">
+                <h2 className="col-12">Humidity</h2>
+                <span className="value col-12">{this.state.data.humidity}%</span>
+                <div className="notification-sensor col-12">
+                  <i className="fas fa-check notification-icon col-12"></i>
+                  <span className="col-12">Status</span>
+                </div>
+              </div>
+              <div className="noise sensor col-sm-12 col-md-3 col-lg-3">
+                <h2 className="col-12">Noise</h2>
+                <span className="value col-12">{this.state.data.noise}db</span>
+                <div className="notification-sensor col-12">
+                  <i className="fas fa-times notification-icon col-12"></i>
+                  <span className="col-12">Status</span>
+                </div>
+              </div>
+              <div className="luminosity sensor col-sm-12 col-md-3 col-lg-3">
+                <h2 className="col-12">Luminosity</h2>
+                <span className="value col-12">{this.state.data.illumination}xl</span>
+                <div className="notification-sensor col-12">
+                  <i className="fas fa-exclamation notification-icon"></i>
+                  <span className="col-12">Status</span>
+                </div>
+              </div>
             </div>
           </div>
-          <div className="humidity sensor col-sm-12 col-md-3 col-lg-3">
-            <h2 className="col-12">Humidity</h2>
-            <span className="value col-12">{this.state.data.humidity}%</span>
-            <div className="notification-sensor col-12">
-            <i className="fas fa-check notification-icon col-12"></i>
-              <span className="col-12">Status</span>
-            </div>
-          </div>
-          <div className="noise sensor col-sm-12 col-md-3 col-lg-3">
-          <h2 className="col-12">Noise</h2>
-          <span className="value col-12">{this.state.data.noise}db</span>
-          <div className="notification-sensor col-12">
-          <i className="fas fa-times notification-icon col-12"></i>
-              <span className="col-12">Status</span>
-            </div>
-          </div>
-          <div className="luminosity sensor col-sm-12 col-md-3 col-lg-3">
-          <h2 className="col-12">Luminosity</h2>
-          <span className="value col-12">{this.state.data.illumination}xl</span>
-          <div className="notification-sensor col-12">
-          <i className="fas fa-exclamation notification-icon"></i>
-              <span className="col-12">Status</span>
-            </div>
-          </div>
-          </div>
-        </div>
-        <ChartLines value={{data: this.state.data}}/>
-        <div className="line"></div>
-      </main>
+          <ChartLines value={{ data: this.state.data }} />
+          <div className="line"></div>
+        </main>
         <footer>
           <div className="container final">
             <div className="row">
@@ -180,23 +180,23 @@ class App extends Component {
             </div>
           </div>
           <div className="container">
-          <div className="row bottom-text">
-          <div className="col-12 no-padding">
-            <span className="strong">&copy; 2018 Robert Bosch GmbH</span>
-            <span>Terms of use</span>
-            <span>Legal notice</span>
-            <span>Privacy statement</span>
-            <span>Cookies</span>
-            <span>Privacy Settings</span>
-            </div>
+            <div className="row bottom-text">
+              <div className="col-12 no-padding">
+                <span className="strong">&copy; 2018 Robert Bosch GmbH</span>
+                <span>Terms of use</span>
+                <span>Legal notice</span>
+                <span>Privacy statement</span>
+                <span>Cookies</span>
+                <span>Privacy Settings</span>
+              </div>
             </div>
           </div>
           <img className="img-fluid" src={colores} alt="" />
         </footer>
       </div>
-      
+
     );
   }
 }
-    
-    export default App;
+
+export default App;
